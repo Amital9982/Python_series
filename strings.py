f34c3e35a508bd461763311fe711006fb35a3542
arpit <arpitmittal050603@gmail.com>
2024-07-28 18:42:58 +0530

    learn how to use formatted string

---
+++ strings.py
@@ -0,0 +1,7 @@
+#formatted strings :- in this type of string we dynamically allocate values through variables
+F_name="Mayank"
+L_name="Mittal"
+
+message=f" Hello {F_name} [{L_name}] , You are doing a great job"
+
+print(message)

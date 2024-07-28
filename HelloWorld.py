dd7389eab75ddd49b919382b2c0af96283275877
arpit <arpitmittal050603@gmail.com>
2024-07-28 13:36:59 +0530

    Learnig How to take Input And Print value

---
+++ HelloWorld.py
@@ -0,0 +1,17 @@
+#How to Print Output
+# Print():- It is used show output on the console 
+
+
+print("Hello Arpit")
+
+# now we try to print variable values
+
+x="Mayank"
+print('the value of x :'+ x)
+
+#Now we ar  learning that How to take input from the user
+# input():- we are using input function for taking input from the user in python and input function return some value in program so for that we have to assign a variable which save the return value of input function so that further we can use that value
+
+str=input("what is your name ?  ")
+
+print('Hi '+str)

08cfd3485ff0863e7d3c83d18f0832b3358ee64f
arpit <arpitmittal050603@gmail.com>
2024-07-28 18:25:14 +0530

    learn Typeconversion

---
+++ TypeConversion.py
@@ -0,0 +1,12 @@
+# Here we are learnig how to convert type of the variable 
+#Generally when we take input from the user it take as a string so we have to convert accordint to data type we need
+
+var= input("whats your age? ")
+print( type(var))
+print(var)
+# now we convert the type into int by using int()function
+
+# var1=int(var)
+var1=float(var)
+print(var1)
+print( type(var1))

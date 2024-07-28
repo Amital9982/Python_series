9e64cbd35def3033bd4c1f689909d9e12a5d7f3e
arpit <arpitmittal050603@gmail.com>
2024-07-28 14:48:48 +0530

    learning about quotes used in string

---
+++ quotes.py
@@ -0,0 +1,24 @@
+# Here in this we learning about single quotes(''),double quotes (""),Triple quotes('''''') 
+#These All quotes are used for string 
+
+
+#single quotes
+course='This ia python course'
+print(course)
+#doubled quotes
+cour="This is a beginner course"
+
+# when we want to use quotes , between quotes so we can use like this
+
+str="python's string"
+str2='python by "Arpit" string'
+print(str)
+print(" ")
+print(str2)
+#triple quotes:- It is used to help us to write multiline strings in a single variable
+
+multiline=''' Hello world
+my name is Arpit 
+this python beginner course
+'''
+print(multiline)
